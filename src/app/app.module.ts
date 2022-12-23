2a62f26427016f63e85143ad9be242e8f35f5320
ayush singhal <ayush.dilipsinghal@gmail.com>
2022-12-23 10:48:44 +0530

    day 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PentHouseComponent } from './penthouse/pent-house.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { FlatListComponent } from './flats/flat-list.component';
+
+import {AnimalListComponent} from "./animals/animal-list.component";
+import {FormsModule} from "@angular/forms";
+import { ProductComponent } from './product/product.component';
+import { StarComponent } from './star/star.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PentHouseComponent,
-    WelcomeComponent
-  ],
+    declarations: [
+        AppComponent,
+        PentHouseComponent,
+        WelcomeComponent,
+        FlatListComponent,
+        AnimalListComponent,
+        ProductComponent,
+        StarComponent
+    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/product/iProduct.ts
@@ -0,0 +1,9 @@
+
+export interface IProduct{
+  id:number
+  name:string
+  image:string
+  category:string
+  price:number
+  rating:number
+}
